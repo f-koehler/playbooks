fb286615b8c1047104f72b1642fac9c02fa55e0c
Fabian Koehler <fabian.koehler@proton.me>
2025-09-04 16:19:22 +0800

    feat(nix): add nix shell

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    ansible
+    ansible-lint
+    ansible-language-server
+    yamllint
+  ];
+
+  shellHook = ''
+    echo "Ansible development environment loaded!"
+    echo "Available tools:"
+    echo "- ansible: $(ansible --version | head -n1)"
+    echo "- ansible-lint: $(ansible-lint --version)"
+    echo "- ansible-language-server: $(ansible-language-server --version)"
+    echo "- yamllint: $(yamllint --version)"
+  '';
+}
